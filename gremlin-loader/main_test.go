7aa17d752fbe8af511eb0d027996f9b4eb8fb257
Jean-Philippe Braun <eon@patapon.info>
2017-05-31 09:56:49 +0200

    Add node properties test

--- gremlin-loader/main_test.go
+++ gremlin-loader/main_test.go
@@ -7,7 +7,19 @@ import (
 	"github.com/go-gremlin/gremlin"
 	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/willfaught/gockle"
 )
+
+func checkNode(t *testing.T, query string, bindings gremlin.Bind) []string {
+	var uuids []string
+	results, err := gremlin.Query(query).Bindings(bindings).Exec()
+	if err != nil {
+		t.Errorf("Failed to run query: %s", query)
+		t.SkipNow()
+	}
+	json.Unmarshal(results, &uuids)
+	return uuids
+}
 
 func TestNodeLink(t *testing.T) {
 	var uuids []string
@@ -36,12 +48,7 @@ func TestNodeLink(t *testing.T) {
 	vmi.Create()
 	vmi.CreateLinks()
 
-	results, _ := gremlin.Query("g.V(vn).in('ref').id()").Bindings(
-		gremlin.Bind{
-			"vn": vnUUID,
-		},
-	).Exec()
-	json.Unmarshal(results, &uuids)
+	uuids = checkNode(t, `g.V(uuid).in('ref').id()`, gremlin.Bind{"uuid": vnUUID})
 
 	assert.Equal(t, len(uuids), 1, "One resource must be linked")
 	assert.Equal(t, vmiUUID, uuids[0], "VMI not correctly linked to VN")
@@ -60,12 +67,43 @@ func TestNodeLink(t *testing.T) {
 	})
 	vmi.UpdateLinks()
 
-	results, _ = gremlin.Query("g.V(vmi).both().id()").Bindings(
-		gremlin.Bind{
-			"vmi": vmiUUID,
-		},
-	).Exec()
-	json.Unmarshal(results, &uuids)
+	uuids = checkNode(t, `g.V(uuid).both().id()`, gremlin.Bind{"uuid": vmiUUID})
 
 	assert.Equal(t, len(uuids), 2, "Two resources must be linked")
+}
+
+func TestNodeProperties(t *testing.T) {
+
+	setupGremlin([]string{"ws://localhost:8182/gremlin"})
+
+	nodeUUID := uuid.NewV4().String()
+	query := "SELECT key, column1, value FROM obj_uuid_table WHERE key=?"
+
+	session := &gockle.SessionMock{}
+	session.When("Close").Return()
+	session.When("ScanMapSlice", query, []interface{}{nodeUUID}).Return(
+		[]map[string]interface{}{
+			{"column1": []byte("type"), "value": `"virtual_machine"`},
+			{"column1": []byte("prop:integer"), "value": `12`},
+			{"column1": []byte("prop:string"), "value": `"str"`},
+			{"column1": []byte("prop:list"), "value": `["a", "b", "c"]`},
+			{"column1": []byte("prop:object"), "value": `{"bool": false, "subObject": {"foo": "bar"}}`},
+		},
+		nil,
+	)
+
+	node := getNode(session, nodeUUID)
+	node.Create()
+
+	var uuids []string
+
+	uuids = checkNode(t, `g.V(uuid).has('integer', 12).id()`, gremlin.Bind{"uuid": nodeUUID})
+	assert.Equal(t, nodeUUID, uuids[0])
+	uuids = checkNode(t, `g.V(uuid).has('string', 'str').id()`, gremlin.Bind{"uuid": nodeUUID})
+	assert.Equal(t, nodeUUID, uuids[0])
+	uuids = checkNode(t, `g.V(uuid).has('list', 'a').has('list', 'b').has('list', 'c').id()`, gremlin.Bind{"uuid": nodeUUID})
+	assert.Equal(t, nodeUUID, uuids[0])
+	uuids = checkNode(t, `g.V(uuid).has('object.bool', false).id()`, gremlin.Bind{"uuid": nodeUUID})
+	assert.Equal(t, nodeUUID, uuids[0])
+	uuids = checkNode(t, `g.V(uuid).has('object.subObject.foo', 'bar').id()`, gremlin.Bind{"uuid": nodeUUID})
 }
