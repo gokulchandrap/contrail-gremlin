3fdb14967bfe26f348a8c552cbe1833c77be625c
Jean-Philippe Braun <eon@patapon.info>
2017-10-13 15:54:13 +0200

    Add gremlin-dump command

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ contrail-gremlin
 
 This project contains a set of tools for using the Gremlin server in the context of Contrail:
 
-gremlin-sync: a go program that load and sync the Contrail DB in the Gremlin server
+ * gremlin-sync: a go program that load and sync the Contrail DB in the Gremlin server
+ * gremlin-dump: a go program that dumps the Contrail DB in a GraphSON file that can be loaded by Gremlin server/console
+ * gremlin-fsck: a contrail-api-cli command that runs consistency checks and apply fixes where possible in Contrail
 
-gremlin-fsck: a contrail-api-cli command that runs consistency checks and apply fixes where possible in Contrail
+Binairies are available at https://github.com/eonpatapon/contrail-gremlin-binaries
 
 Run gremlin server
 ------------------
@@ -31,7 +33,7 @@ Build (you need golang 1.7)
 Load and sync Contrail DB in gremlin server
 
     ./gremlin-sync --rabbit <server> --rabbit-vhost <vhost> --rabbit-user <user> --rabbit-password <pass> --cassandra <server> --gremlin localhost:8182
-    
+
 The DB is reloaded completely every 30min
 
 Connect to server with Gremlin console
@@ -50,6 +52,78 @@ Then you can query the remote graph with ':>'
 
     :> g.V()
     :> g.V().hasLabel('virtual_network')
+
+Using gremlin-dump
+------------------
+
+    ./gremlin-dump --cassandra localhost dump.json
+
+Then you can load the dump in the gremlin console:
+
+    bin/gremlin.sh
+             \,,,/
+             (o o)
+    -----oOOo-(3)-oOOo-----
+    plugin activated: tinkerpop.server
+    plugin activated: tinkerpop.utilities
+    plugin activated: tinkerpop.tinkergraph
+    gremlin> graph = TinkerGraph.open()
+    ==>tinkergraph[vertices:0 edges:0]
+    gremlin> graph.io(IoCore.graphson()).readGraph("/path/to/dump.json");
+    ==>null
+    gremlin> graph
+    ==>tinkergraph[vertices:1735 edges:1349]
+    gremlin> g = graph.traversal()
+    ==>graphtraversalsource[tinkergraph[vertices:1735 edges:1349], standard]
+    gremlin> g.V().hasLabel('virtual_network').count()
+    ==>109
+
+The dump contains all contrail resources including incomplete or missing ones. Incomplete are resources that have no type or fq_name or id_perms properties. Missing are resources that are not in the DB but still referenced by other resources. Incomplete resources have an `_incomplete` property, missings ones have a `_missing` property.
+
+# Use cases
+
+## Broken references
+
+For example to get resources that reference missing references we can do:
+
+    gremlin> g.V().hasNot('_missing').both().has('_missing').path()
+    ==>[v[58b78816-8df1-4c4f-9854-d618732b984d],v[d110bfaa-6a8d-49e0-b527-cf166eef2329]]
+    ==>[v[3285ef82-5e74-433a-9d7b-7a39cc5bce81],v[b3b48af9-ce7b-43bd-94b5-7ad4e393e66b]]
+    [...]
+
+This builds a list of paths where each path is 2 resources. The first one exists in the Contrail DB and it is linked to the second one that is not in the Contrail DB because we match on the `_missing` property.
+
+From the path we can get the type of resources by augmenting the query to:
+
+    gremlin> g.V().hasNot('_missing').both().has('_missing').path().map(unfold().union(label(), id()).fold())
+    ==>[routing_instance,58b78816-8df1-4c4f-9854-d618732b984d,virtual_network,d110bfaa-6a8d-49e0-b527-cf166eef2329]
+    ==>[routing_instance,3285ef82-5e74-433a-9d7b-7a39cc5bce81,virtual_network,b3b48af9-ce7b-43bd-94b5-7ad4e393e66b]
+    [...]
+
+In this case the parent link between some RI and VN is broken because the VN is not in the DB anymore but the RI is still referencing it. Let's check the cassandra DB to confirm this:
+
+    cqlsh
+    cqlsh> use config_db_uuid ;
+    cqlsh:config_db_uuid> SELECT blobAsText(key), blobAsText(column1) FROM obj_uuid_table WHERE key = textAsBlob('58b78816-8df1-4c4f-9854-d618732b984d');
+
+     blobAsText(key)                      | blobAsText(column1)
+    --------------------------------------+-------------------------------------------------------------
+     58b78816-8df1-4c4f-9854-d618732b984d |                                                     fq_name
+     58b78816-8df1-4c4f-9854-d618732b984d | parent:virtual_network:d110bfaa-6a8d-49e0-b527-cf166eef2329
+     58b78816-8df1-4c4f-9854-d618732b984d |                                                 parent_type
+     58b78816-8df1-4c4f-9854-d618732b984d |                                           prop:display_name
+     58b78816-8df1-4c4f-9854-d618732b984d |                                               prop:id_perms
+     58b78816-8df1-4c4f-9854-d618732b984d |                            prop:routing_instance_is_default
+     58b78816-8df1-4c4f-9854-d618732b984d |       ref:route_target:79cf45c3-457a-4269-8fc4-bf41d5525110
+     58b78816-8df1-4c4f-9854-d618732b984d |                                                        type
+
+    (8 rows)
+
+    cqlsh:config_db_uuid> SELECT blobAsText(key), blobAsText(column1) FROM obj_uuid_table WHERE key = textAsBlob('d110bfaa-6a8d-49e0-b527-cf166eef2329');
+
+    (0 rows)
+
+    cqlsh:config_db_uuid>
 
 Run gremlin-fsck
 ----------------
---
+++ gremlin-dump/main.go
@@ -0,0 +1,510 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/Jeffail/gabs"
+	"github.com/gocql/gocql"
+	"github.com/jawher/mow.cli"
+	logging "github.com/op/go-logging"
+	"github.com/willfaught/gockle"
+)
+
+var (
+	log    = logging.MustGetLogger("gremlin-loader")
+	format = logging.MustStringFormatter(
+		`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`)
+)
+
+const (
+	// Readers numbers of workers reading cassandra resources
+	Readers = 10
+)
+
+const (
+	DumpStart = iota
+	ResourceRead
+	NormalVertex
+	MissingVertex
+	DuplicateVertex
+	IncompleteVertex
+	DumpEnd
+)
+
+type VertexEdge struct {
+	vID     string
+	oVertex Vertex
+}
+
+type Property struct {
+	ID    int64       `json:"id"`
+	Value interface{} `json:"value"`
+}
+
+type Edge struct {
+	ID   int64  `json:"id"`
+	OutV string `json:"outV,omitempty"`
+	InV  string `json:"inV,omitempty"`
+}
+
+type Vertex struct {
+	ID         string                `json:"id"`
+	Label      string                `json:"label"`
+	Properties map[string][]Property `json:"properties,omitempty"`
+	InE        map[string][]Edge     `json:"inE,omitempty"`
+	OutE       map[string][]Edge     `json:"outE,omitempty"`
+}
+
+func (v *Vertex) AddProperties(prefix string, c *gabs.Container, l *Dumper) {
+	if _, ok := c.Data().([]interface{}); ok {
+		childs, _ := c.Children()
+		for _, child := range childs {
+			v.AddProperties(prefix, child, l)
+		}
+		return
+	}
+	if _, ok := c.Data().(map[string]interface{}); ok {
+		childs, _ := c.ChildrenMap()
+		for key, child := range childs {
+			v.AddProperties(prefix+"."+key, child, l)
+		}
+		return
+	}
+	if str, ok := c.Data().(string); ok {
+		v.AddProperty(prefix, str, l)
+		return
+	}
+	if num, ok := c.Data().(float64); ok {
+		v.AddProperty(prefix, num, l)
+		return
+	}
+	if boul, ok := c.Data().(bool); ok {
+		v.AddProperty(prefix, boul, l)
+		return
+	}
+	v.AddProperty(prefix, "null", l)
+}
+
+func (v *Vertex) AddProperty(prefix string, value interface{}, l *Dumper) {
+	if props, ok := v.Properties[prefix]; !ok {
+		v.Properties[prefix] = []Property{
+			Property{ID: atomic.AddInt64(l.propID, 1), Value: value},
+		}
+	} else {
+		currentValue := props[0].Value
+		switch currentValue.(type) {
+		case []interface{}:
+			v.Properties[prefix][0].Value = append(currentValue.([]interface{}), value)
+		default:
+			v.Properties[prefix][0].Value = []interface{}{currentValue, value}
+		}
+	}
+}
+
+func setupCassandra(cassandraCluster []string) (gockle.Session, error) {
+	log.Notice("Connecting to Cassandra...")
+	cluster := gocql.NewCluster(cassandraCluster...)
+	cluster.Keyspace = "config_db_uuid"
+	cluster.Consistency = gocql.Quorum
+	cluster.Timeout = 2000 * time.Millisecond
+	cluster.DisableInitialHostLookup = true
+	session, err := cluster.CreateSession()
+	if err != nil {
+		return nil, err
+	}
+	mockableSession := gockle.NewSession(session)
+	log.Notice("Connected.")
+	return mockableSession, err
+}
+
+func setup(cassandraCluster []string, filePath string) {
+	var (
+		session gockle.Session
+		err     error
+	)
+
+	backend := logging.NewLogBackend(os.Stderr, "", 0)
+	backendFormatter := logging.NewBackendFormatter(backend, format)
+	logging.SetBackend(backendFormatter)
+
+	session, err = setupCassandra(cassandraCluster)
+	if err != nil {
+		log.Fatalf("Failed to connect to Cassandra: %s", err)
+	}
+	defer session.Close()
+
+	load(session, filePath)
+}
+
+func (l *Dumper) getEdgeID(edgeUUID string) int64 {
+	l.Lock()
+	defer l.Unlock()
+	if _, ok := l.edgeIDs[edgeUUID]; !ok {
+		l.edgeIDs[edgeUUID] = atomic.AddInt64(l.edgeID, 1)
+	}
+	return l.edgeIDs[edgeUUID]
+}
+
+func (l *Dumper) getContrailResource(session gockle.Session, uuid string) (Vertex, error) {
+	var (
+		column1   string
+		valueJSON []byte
+	)
+	rows, err := session.ScanMapSlice(`SELECT key, column1, value FROM obj_uuid_table WHERE key=?`, uuid)
+	if err != nil {
+		log.Criticalf("[%s] %s", uuid, err)
+		return Vertex{}, err
+	}
+	vertex := Vertex{
+		ID:         uuid,
+		Properties: map[string][]Property{},
+		InE:        map[string][]Edge{},
+		OutE:       map[string][]Edge{},
+	}
+	for _, row := range rows {
+		column1 = string(row["column1"].([]byte))
+		valueJSON = []byte(row["value"].(string))
+		split := strings.Split(column1, ":")
+		switch split[0] {
+		case "ref":
+		case "parent":
+			label := split[0]
+			edgeUUID := uuid + "-" + split[2]
+			id := l.getEdgeID(edgeUUID)
+			edge := Edge{ID: id, InV: split[2]}
+			if _, ok := vertex.OutE[label]; !ok {
+				vertex.OutE[label] = []Edge{edge}
+			} else {
+				vertex.OutE[label] = append(vertex.OutE[label], edge)
+			}
+			ve := VertexEdge{
+				vID: uuid,
+				oVertex: Vertex{
+					ID:    split[2],
+					Label: split[1],
+					InE: map[string][]Edge{
+						label: []Edge{Edge{ID: id, OutV: uuid}},
+					},
+				},
+			}
+			l.seen <- ve
+		case "backref":
+		case "children":
+			var label string
+			if split[0] == "backref" {
+				label = "ref"
+			} else {
+				label = "parent"
+			}
+			edgeUUID := split[2] + "-" + uuid
+			id := l.getEdgeID(edgeUUID)
+			edge := Edge{ID: id, OutV: split[2]}
+			if _, ok := vertex.InE[label]; !ok {
+				vertex.InE[label] = []Edge{edge}
+			} else {
+				vertex.InE[label] = append(vertex.InE[label], edge)
+			}
+			ve := VertexEdge{
+				vID: uuid,
+				oVertex: Vertex{
+					ID:    split[2],
+					Label: split[1],
+					OutE: map[string][]Edge{
+						label: []Edge{Edge{ID: id, InV: uuid}},
+					},
+				},
+			}
+			l.seen <- ve
+		case "type":
+			var value string
+			json.Unmarshal(valueJSON, &value)
+			vertex.Label = value
+		case "fq_name":
+			var value []string
+			json.Unmarshal(valueJSON, &value)
+			vertex.AddProperty("fq_name", value, l)
+		case "prop":
+			value, err := gabs.ParseJSON(valueJSON)
+			if err != nil {
+				log.Criticalf("Failed to parse %v", string(valueJSON))
+			} else {
+				vertex.AddProperties(split[1], value, l)
+			}
+		}
+	}
+
+	if len(vertex.Label) == 0 {
+		vertex.Label = "_incomplete"
+		vertex.AddProperty("_incomplete", true, l)
+	}
+	if _, ok := vertex.Properties["fq_name"]; !ok {
+		vertex.AddProperty("_incomplete", true, l)
+	}
+	if _, ok := vertex.Properties["id_perms.created"]; !ok {
+		vertex.AddProperty("_incomplete", true, l)
+	}
+
+	return vertex, nil
+}
+
+type Dumper struct {
+	count     chan int64
+	wgCount   *sync.WaitGroup
+	wgRead    *sync.WaitGroup
+	wgWrite   *sync.WaitGroup
+	wgChecker *sync.WaitGroup
+	uuids     chan string
+	seen      chan VertexEdge
+	write     chan Vertex
+	session   gockle.Session
+	propID    *int64
+	edgeID    *int64
+	edgeIDs   map[string]int64
+	filePath  string
+	sync.Mutex
+}
+
+func NewLoader(session gockle.Session, filePath string) *Dumper {
+	return &Dumper{
+		count:     make(chan int64),
+		uuids:     make(chan string),
+		seen:      make(chan VertexEdge),
+		write:     make(chan Vertex),
+		wgCount:   &sync.WaitGroup{},
+		wgRead:    &sync.WaitGroup{},
+		wgWrite:   &sync.WaitGroup{},
+		wgChecker: &sync.WaitGroup{},
+		session:   session,
+		propID:    new(int64),
+		edgeID:    new(int64),
+		edgeIDs:   map[string]int64{},
+		filePath:  filePath,
+	}
+}
+
+func (l *Dumper) reporter() {
+	l.wgCount.Add(1)
+	defer l.wgCount.Done()
+	readCount := 0
+	normalCount := 0
+	missingCount := 0
+	duplicateCount := 0
+	incompleteCount := 0
+
+	dumpStatus := `W`
+
+	for c := range l.count {
+		switch c {
+		case ResourceRead:
+			readCount++
+		case NormalVertex:
+			normalCount++
+		case MissingVertex:
+			missingCount++
+		case DuplicateVertex:
+			duplicateCount++
+		case IncompleteVertex:
+			incompleteCount++
+		case DumpStart:
+			dumpStatus = `R`
+		case DumpEnd:
+			dumpStatus = `D`
+		}
+		fmt.Printf("\rProcessing nodes [read:%d correct:%d incomplete:%d missing:%d dup:%d] %s",
+			readCount, normalCount, incompleteCount, missingCount, duplicateCount, dumpStatus)
+	}
+	fmt.Println()
+}
+
+func (l *Dumper) writer() {
+	l.wgWrite.Add(1)
+	defer l.wgWrite.Done()
+	f, err := os.Create(l.filePath)
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+	// To handle duplicate uuids in the fq_name table
+	written := make(map[string]bool)
+	for v := range l.write {
+		if _, ok := written[v.ID]; ok {
+			l.count <- DuplicateVertex
+			continue
+		} else {
+			written[v.ID] = true
+		}
+		vJSON, err := json.Marshal(v)
+		if err != nil {
+			log.Criticalf("Failed to convert %v to json", v)
+		} else {
+			_, err := f.Write(vJSON)
+			if err != nil {
+				log.Criticalf("Failed to write %v to file", v)
+			} else {
+				if _, ok := v.Properties["_missing"]; ok {
+					l.count <- MissingVertex
+				} else if _, ok := v.Properties["_incomplete"]; ok {
+					l.count <- IncompleteVertex
+				} else {
+					l.count <- NormalVertex
+				}
+			}
+			f.WriteString("\n")
+		}
+	}
+	f.Sync()
+}
+
+func (l *Dumper) getContrailResources() error {
+	var (
+		column1 string
+	)
+	r := l.session.ScanIterator(`SELECT column1 FROM obj_fq_name_table`)
+	for r.Scan(&column1) {
+		parts := strings.Split(column1, ":")
+		uuid := parts[len(parts)-1]
+		l.uuids <- uuid
+	}
+	if err := r.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (l *Dumper) getNodes() (err error) {
+	defer close(l.uuids)
+	l.count <- DumpStart
+	err = l.getContrailResources()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (l *Dumper) reader() {
+	l.wgRead.Add(1)
+	defer l.wgRead.Done()
+	for uuid := range l.uuids {
+		vertex, err := l.getContrailResource(l.session, uuid)
+		if err != nil {
+			log.Warningf("%s", err)
+		} else {
+			l.count <- ResourceRead
+			l.write <- vertex
+		}
+	}
+}
+
+func (l *Dumper) checker() {
+	l.wgChecker.Add(1)
+	defer l.wgChecker.Done()
+	seen := make(map[string]interface{})
+	for ve := range l.seen {
+		seen[ve.vID] = true
+		if _, ok := seen[ve.oVertex.ID]; !ok {
+			seen[ve.oVertex.ID] = ve.oVertex
+		}
+	}
+	missing := make(map[string][]Vertex)
+	for _, v := range seen {
+		switch v.(type) {
+		case Vertex:
+			v := v.(Vertex)
+			if _, ok := missing[v.ID]; !ok {
+				missing[v.ID] = []Vertex{v}
+			} else {
+				missing[v.ID] = append(missing[v.ID], v)
+			}
+		}
+	}
+	for _, vs := range missing {
+		v := Vertex{
+			ID:    vs[0].ID,
+			Label: vs[0].Label,
+			Properties: map[string][]Property{
+				"_missing": []Property{
+					Property{ID: atomic.AddInt64(l.propID, 1), Value: true},
+				},
+			},
+			InE:  map[string][]Edge{},
+			OutE: map[string][]Edge{},
+		}
+		// merge all edges
+		for _, v2 := range vs {
+			for label, edges := range v2.InE {
+				for _, e := range edges {
+					v.InE[label] = append(v.InE[label], e)
+				}
+			}
+			for label, edges := range v2.OutE {
+				for _, e := range edges {
+					v.OutE[label] = append(v.OutE[label], e)
+				}
+			}
+		}
+		l.write <- v
+	}
+}
+
+func (l *Dumper) setupReaders() {
+	for w := 1; w <= Readers; w++ {
+		go l.reader()
+	}
+}
+
+func (l *Dumper) teardown() {
+	l.wgRead.Wait()
+	close(l.seen)
+	l.wgChecker.Wait()
+	l.count <- DumpEnd
+	close(l.write)
+	l.wgWrite.Wait()
+	close(l.count)
+	l.wgCount.Wait()
+	// just to line up the display
+	time.Sleep(time.Second * 1)
+}
+
+func (l *Dumper) Run() {
+	go l.reporter()
+	go l.writer()
+	go l.checker()
+
+	l.setupReaders()
+	err := l.getNodes()
+	if err != nil {
+		log.Criticalf("Sync failed: %s", err)
+		return
+	}
+	l.teardown()
+}
+
+func load(session gockle.Session, filePath string) {
+	loader := NewLoader(session, filePath)
+	loader.Run()
+}
+
+func main() {
+	app := cli.App("gremlin-loader", "Load and Sync Contrail DB in Gremlin Server")
+	cassandraSrvs := app.Strings(cli.StringsOpt{
+		Name:   "cassandra",
+		Value:  []string{"localhost"},
+		Desc:   "list of host of cassandra nodes, uses CQL port 9042",
+		EnvVar: "GREMLIN_DUMP_CASSANDRA_SERVERS",
+	})
+	filePath := app.String(cli.StringArg{
+		Name:   "DST",
+		Desc:   "Output file path",
+		EnvVar: "GREMLIN_DUMP_FILE_PATH",
+	})
+	app.Action = func() {
+		setup(*cassandraSrvs, *filePath)
+	}
+	app.Run(os.Args)
+}
